2d7ac42516eff20b39a180e52444c2532a63c6e6
geumcomo <162772763+geumcomo@users.noreply.github.com>
2024-09-04 09:08:26 +0900

    feat: 관리자 페이지 생성 (패키지 추가) (#25)
    * feat: 관리자페이지 레이아웃 페이지 생성
    
    * feat:   "@mui/icons-material": "^6.0.1",    "bootstrap": "^5.3.3",    "react-bootstrap": "^2.10.4",    "react-chartjs-2": "^5.2.0" 패키지 추가
    
    * feat: 관리자페이지 레이아웃 페이지 생성
    
    * feat: 관리자페이지 메인 대시보드 생성
    
    * feat: 관리자페이지 레이아웃 스타일 생성
    
    * feat: 관리자페이지 기업관리 페이지 생성
    
    * feat: 관리자페이지 회원관리 페이지 생성
    
    * feat: 관리자페이지 공고관리 페이지 생성

---
+++ src/components/admin/footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <footer className="sticky-footer bg-white">
+            <div className="container my-auto">
+                <div className="copyright text-center my-auto">
+                    <span>Copyright &copy; Job-Jam 2024</span>
+                </div>
+            </div>
+        </footer>
+    )
+}
+ 
