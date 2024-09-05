13da45e2fa2bd9cbf9d2ba0f7bf4181c9f7be0a5
Kim YuBin <dbqks56231106@gmail.com>
2024-09-05 12:11:30 +0900

    Feat : 개인, 기업 회원가입 기능 추가 (#35)
    * Feat : 회원가입, 로그인 코드 작업중
    
    * Feat : 개인회원, 기업회원 API 요청 나누기
    
    * Remove : 폴더 구조 변경으로 인한 파일 이동 & 삭제
    
    * Feat : API 데이터 구조에 맞게 데이터 설정
    
    * Docs : 간단한 로고 추가
    
    * Fix : 폴더 구조 변경으로 파일  이동
    
    * Feat : 개인, 기업 회원가입 기능 추가

---
+++ src/app/(FO)/reg/company/form.js
@@ -0,0 +1,33 @@
+import { useContext } from 'react';
+import JoinButton from '../component/JoinButton';
+import Section from '../component/Section';
+import CompanyForm from '../component/form/CompanyForm';
+import CompanyManagerForm from '../component/form/CompanyManagerForm';
+import TermsAgreementForm from '../component/form/TermsAgreementForm';
+import UserForm from '../component/form/UserForm';
+import { useFormContext } from '../page';
+
+export default function CompanyReg() {
+  const { handleSubmit, reset } = useContext(useFormContext);
+  
+  const onSubmit = (data) => {
+    console.log(data);
+    reset();
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Section title="로그인 정보">
+        <UserForm/>
+      </Section>
+      <Section title="기업회원 기본정보">
+        <CompanyForm/>
+      </Section>
+      <Section title="기업 인사담당자 정보">
+        <CompanyManagerForm/>
+      </Section>
+      <TermsAgreementForm/>
+      <JoinButton/>
+    </form>
+  );
+}
