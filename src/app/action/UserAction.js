331c1aa0ec533e65d5898b706f32b8594b412f0d
Kim YuBin <dbqks56231106@gmail.com>
2024-09-05 09:10:28 +0900

    Feat : 개인회원, 기업회원 API 요청 나누기

--- src/app/action/UserAction.js
+++ src/app/action/UserAction.js
@@ -24,7 +24,31 @@ export async function login(formdata) {
     }
 }
 
-export async function join(formdata) {
+export async function personalJoin(formdata) {
+    const API_URL = "/api/join"
+    
+    const result = axios.post(API_URL, {
+        email: formdata.email,
+        password: formdata.password
+    })
+    
+    switch ((await result).status) {
+        case 200:
+            return "로그인 성공"
+            break;
+        case 208:
+            return "이메일이 존재하지 않습니다."
+            break;
+        case 209:
+            return "비밀번호를 확인해주세요."
+            break;
+        default:
+            return "서버 문제 발생"
+            break;
+    }
+}
+
+export async function companyJoin(formdata) {
     const API_URL = "/api/join"
     
     const result = axios.post(API_URL, {
