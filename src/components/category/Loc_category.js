1dfa01a71221626cdc195be9efb8831a5fe46f27
DESKTOP-P15R8BC\JY <wjddl5796@naver.com>
2024-09-08 17:27:29 +0900

    Feat : 지역 카테고리 관리 페이지 부분

--- src/components/category/Loc_category.js
+++ src/components/category/Loc_category.js
@@ -1,74 +1,88 @@
-'use client';
-import { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import { Tabs, Tab, Typography, Box } from '@mui/material';
+"use client";
+import { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import { Tabs, Tab, Typography, Box } from "@mui/material";
+import axios from "axios";
 
 function TabPanel(props) {
-	const { children, value, index, ...other } = props;
+  const { children, value, index, ...other } = props;
 
-	return (
-		<div role='tabpanel' hidden={value !== index} id={`vertical-tabpanel-${index}`} aria-labelledby={`vertical-tab-${index}`} {...other}>
-			{value === index && (
-				<Box sx={{ p: 3 }}>
-					<Typography>{children}</Typography>
-				</Box>
-			)}
-		</div>
-	);
+  return (
+    <div role="tabpanel" hidden={value !== index} id={`vertical-tabpanel-${index}`} aria-labelledby={`vertical-tab-${index}`} {...other}>
+      {value === index && (
+        <Box sx={{ p: 4, height: "100%", width: "100%" }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
 }
 
 TabPanel.propTypes = {
-	children: PropTypes.node,
-	index: PropTypes.number.isRequired,
-	value: PropTypes.number.isRequired,
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
 };
 
 function a11yProps(index) {
-	return {
-		'id': `vertical-tab-${index}`,
-		'aria-controls': `vertical-tabpanel-${index}`,
-	};
+  return {
+    id: `vertical-tab-${index}`,
+    "aria-controls": `vertical-tabpanel-${index}`,
+  };
 }
 
 export default function VerticalTabs() {
-	const [value, setValue] = useState(0);
+  const [value, setValue] = useState(0);
 
-	const handleChange = (event, newValue) => {
-		setValue(newValue);
-	};
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
-	return (
-		<Box sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', height: 300 }}>
-			<Tabs orientation='vertical' variant='scrollable' value={value} onChange={handleChange} aria-label='Vertical tabs example' sx={{ borderRight: 1, borderColor: 'divider' }}>
-				<Tab label='Item One' {...a11yProps(0)} />
-				<Tab label='Item Two' {...a11yProps(1)} />
-				<Tab label='Item Three' {...a11yProps(2)} />
-				<Tab label='Item Four' {...a11yProps(3)} />
-				<Tab label='Item Five' {...a11yProps(4)} />
-				<Tab label='Item Six' {...a11yProps(5)} />
-				<Tab label='Item Seven' {...a11yProps(6)} />
-			</Tabs>
-			<TabPanel value={value} index={0}>
-				Item One
-			</TabPanel>
-			<TabPanel value={value} index={1}>
-				Item Two
-			</TabPanel>
-			<TabPanel value={value} index={2}>
-				Item Three
-			</TabPanel>
-			<TabPanel value={value} index={3}>
-				Item Four
-			</TabPanel>
-			<TabPanel value={value} index={4}>
-				Item Five
-			</TabPanel>
-			<TabPanel value={value} index={5}>
-				Item Six
-			</TabPanel>
-			<TabPanel value={value} index={6}>
-				Item Seven
-			</TabPanel>
-		</Box>
-	);
+  // ========================
+  const [loc_do, setLoc_do] = useState([]);
+  const [loc_gusi, setLoc_gusi] = useState([]);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  function getData() {
+    axios.get("/api/category/loc").then((res) => {
+      if (res.data) {
+        setLoc_do(res.data);
+        getGuSi(res.data.id[0]);
+      }
+    });
+  }
+
+  function getGuSi(id) {
+    axios.get("/api/category/locGuSi", { params: { ldIdx: id } }).then((res) => {
+      setLoc_gusi(res.data);
+    });
+  }
+
+  // =========================
+
+  return (
+    <Box sx={{ flexGrow: 1, bgcolor: "background.paper", display: "flex", height: 500 }}>
+      <Tabs
+        orientation="vertical"
+        variant="scrollable"
+        value={value}
+        onChange={handleChange}
+        aria-label="Vertical tabs example"
+        sx={{ borderRight: 1, borderColor: "divider", width: 200 }}
+      >
+        {loc_do.map((row, index) => (
+          <Tab label={row.name} {...a11yProps(index)} key={row.id} />
+        ))}
+      </Tabs>
+
+      {loc_do.map((row, index) => (
+        <TabPanel value={value} index={index} key={index}>
+          {!loc_gusi.length ? <Typography>항목 없음</Typography> : loc_gusi.map((gusi, idx) => <Typography key={idx}>{gusi.lgName}</Typography>)}
+        </TabPanel>
+      ))}
+    </Box>
+  );
 }
