d4f6032f283274da867a1edbcd35c73f8206e7d4
jong-jang <jjh0299@gmail.com>
2024-09-09 12:49:56 +0900

    Fix : 아이콘 버튼 기본 기능 상속으로 수정

--- src/components/button/IconButton.js
+++ src/components/button/IconButton.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-export default function IconButton({children}) {
+export default function IconButton(props) {
     return (
         <button
+            {...props}
             className="middle size-6 text-2xl none center flex items-center justify-center rounded-lg font-bold uppercase text-black disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-        >{children}
+        >{props.children}
         </button>
     )
 }
