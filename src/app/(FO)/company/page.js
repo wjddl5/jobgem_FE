16c0077bff8eadccd4662c1b56e90c6f26bdadeb
jong-jang <jjh0299@gmail.com>
2024-09-09 16:17:25 +0900

    Fix : 차단목록 페이징 빼고 가장 최근 3개만 불러오게 수정

--- src/app/(FO)/company/page.js
+++ src/app/(FO)/company/page.js
@@ -1,6 +1,6 @@
 "use client"; // 클라이언트 측 렌더링 활성화
 
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Button from "@/components/button/Button";
 import { DiAptana } from "react-icons/di";
 import IconButton from "@/components/button/IconButton";
@@ -8,27 +8,22 @@ import Image from "next/image";
 import Table from "@/components/table/Table";
 import Link from "next/link";
 import axios from "axios";
-import Pagination from "@/components/pagination/Pagination";
 
 export default function Page() {
-    const [company, setCompany] = useState(null); // 회사 데이터를 상태로 관리
-    const [blockPage, setBlockPage] = useState(0); // 페이지 번호 상태 관리
+    const [companyData, setCompanyData] = useState();
 
     // 데이터를 가져오는 함수
-    const getPage = async (id, blockPage) => {
-        const company = await axios(`/api/company?id=${id}&blockPage=${blockPage}`);
-        return company.data;
+    const getData = () => {
+        axios.get(`/api/company?id=${1}`).then((res) => {
+            setCompanyData(res.data);
+        })
     }
     // 페이지 번호가 변경될 때마다 데이터를 가져옴
     useEffect(() => {
-        async function fetchData() {
-            const data = await getPage(1, blockPage);
-            setCompany(data);
-        }
-        fetchData();
-    }, [blockPage]);
+        getData();
+    }, []);
 
-    if (!company) return <div className='flex justify-center'><img src='/img/loading.gif' alt='로딩' /></div>;
+    if (!companyData) return <div className='flex justify-center'><img src='/img/loading.gif' alt='로딩' /></div>;
 
     return (
         <>
@@ -55,11 +50,11 @@ export default function Page() {
                         <div className='flex justify-between'>
                             <div className="text-center p-4">
                                 <p className="mb-2">매출액</p>
-                                <p className="text-2xl font-bold">{company.company.coSales / 100000000} 억</p>
+                                <p className="text-2xl font-bold">{companyData.company.coSales / 100000000} 억</p>
                             </div>
                             <div className="text-center p-4">
                                 <p className="mb-2">사원수</p>
-                                <p className="text-2xl font-bold">{company.company.coEmployee}명</p>
+                                <p className="text-2xl font-bold">{companyData.company.coEmployee}명</p>
                             </div>
                             <div className="text-center p-4">
                                 <p className="mb-2">산업</p>
@@ -67,11 +62,11 @@ export default function Page() {
                             </div>
                             <div className="text-center p-4">
                                 <p className="mb-2">설립</p>
-                                <p className="text-2xl font-bold">{company.company.coOpen.slice(0,4)}년</p>
+                                <p className="text-2xl font-bold">{companyData.company.coOpen.slice(0,4)}년</p>
                             </div>
                             <div className="text-center p-4">
                                 <p className="mb-2">기업형태</p>
-                                <p className="text-2xl font-bold">{company.company.coType}</p>
+                                <p className="text-2xl font-bold">{companyData.company.coType}</p>
                             </div>
                         </div>
                     </div>
@@ -81,11 +76,11 @@ export default function Page() {
                         <h2 className="text-lg font-bold mb-4">일반 채용 현황</h2>
                         <div className="flex justify-between mb-4">
                             <div className="text-center">
-                                <Link href='/company' className="text-2xl font-bold text-blue-600 underline text-center">{company.postCount}</Link>
+                                <Link href='/company' className="text-2xl font-bold text-blue-600 underline text-center">{companyData.postCount}</Link>
                                 <p>진행중 공고</p>
                             </div>
                             <div className="text-center">
-                                <Link href='/company' className="text-2xl font-bold text-blue-600 underline text-center">{company.noPostCount}</Link>
+                                <Link href='/company' className="text-2xl font-bold text-blue-600 underline text-center">{companyData.noPostCount}</Link>
                                 <p>마감된 공고</p>
                             </div>
                         </div>
@@ -99,11 +94,11 @@ export default function Page() {
                         <h2 className="text-lg font-bold mb-4">후기 현황</h2>
                         <div className="flex justify-between mb-4">
                             <div className="text-center">
-                                <Link href='/company' className="text-2xl font-bold text-blue-600 underline text-center">{company.reviewCount}</Link>
+                                <Link href='/company' className="text-2xl font-bold text-blue-600 underline text-center">{companyData.reviewCount}</Link>
                                 <p>기업 후기</p>
                             </div>
                             <div className="text-center">
-                                <Link href='/company' className="text-2xl font-bold text-blue-600 underline text-center">{company.interviewCount}</Link>
+                                <Link href='/company' className="text-2xl font-bold text-blue-600 underline text-center">{companyData.interviewCount}</Link>
                                 <p>면접 후기</p>
                             </div>
                         </div>
@@ -116,11 +111,11 @@ export default function Page() {
                         <h2 className="text-lg font-bold mb-4">인재 현황</h2>
                         <div className="flex felx-co justify-between mb-4">
                             <div className="text-center">
-                                <Link href='/company/talent/fit' className="text-2xl font-bold text-blue-600 underline text-center">{company.fitJobseekerCount}</Link>
+                                <Link href='/company/talent/fit' className="text-2xl font-bold text-blue-600 underline text-center">{companyData.fitJobseekerCount}</Link>
                                 <p>인재 추천</p>
                             </div>
                             <div className="text-center">
-                                <Link href='/company/talent/wish' className="text-2xl font-bold text-blue-600 underline text-center">{company.talentCount}</Link>
+                                <Link href='/company/talent/wish' className="text-2xl font-bold text-blue-600 underline text-center">{companyData.talentCount}</Link>
                                 <p>찜한 인재</p>
                             </div>
                         </div>
@@ -133,22 +128,18 @@ export default function Page() {
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
                     <div className="bg-white p-6 rounded-md shadow">
                         <div className='mb-4 flex justify-between'>
-                            <h2 className="text-lg font-bold">차단 목록</h2>
+                            <h2 className="text-lg font-bold">차단 목록<span className='text-xs font-gray-500 font-light'>( 최신순 )</span></h2>
                             <div className="hover:rotate-90 transition duration-200 ease-in-out h-6">
                                 <Link href='/company/black'><IconButton><DiAptana/></IconButton></Link>
                             </div>
                         </div>
-                        <Table list={company.blockList.content} headers={['번호', '이름', '차단일자', '내용']} isNumber={true}/>
-                        <Pagination
-                            totalPages={company.blockList.totalPages}
-                            currentPage={blockPage}
-                            onPageChange={(page) => setBlockPage(page)}
-                        />
+                        <Table list={companyData.blockList} headers={['번호', '이름', '차단일자', '내용']} isNumber={true}/>
                     </div>
 
                     <div className="bg-white p-6 rounded-md shadow">
                         <div className='mb-4 flex justify-between'>
-                            <h2 className="text-lg font-bold">채팅 목록</h2>
+                            <h2 className="text-lg font-bold">채팅 목록<span className='text-xs font-gray-500 font-light'>( 최신순 )</span>
+                            </h2>
                             <div className="hover:rotate-90 transition duration-200 ease-in-out h-6">
                                 <IconButton><DiAptana/></IconButton>
                             </div>
