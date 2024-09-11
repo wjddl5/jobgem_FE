0457ccb6254b85ecb1e640e84c8fa324c6db620b
jong-jang <jjh0299@gmail.com>
2024-09-11 14:07:47 +0900

    Feat : 기업면접 리뷰 조회

--- src/components/pagination/Pagination.js
+++ src/components/pagination/Pagination.js
@@ -1,21 +1,29 @@
 import React from 'react';
 
-function Pagination({ totalPages, currentPage, onPageChange }) {
-    const goToPreviousPage = () => {
-        if (currentPage > 0) {
-            onPageChange(currentPage - 1);
+function Pagination({ totalPages, currentPage, setLoadPage }) {
+    const pagesPerGroup = 5;
+    // Calculate the block range of pages to display
+    const startPage = Math.floor(currentPage / pagesPerGroup) * pagesPerGroup;
+    const endPage = Math.min(startPage + pagesPerGroup, totalPages);
+
+    // Calculate the current block and total blocks
+    const currentBlock = Math.floor(currentPage / pagesPerGroup);
+    const totalBlocks = Math.ceil(totalPages / pagesPerGroup);
+
+    const goToPreviousBlock = () => {
+        if (currentBlock > 0) {
+            setLoadPage((currentBlock-1) * pagesPerGroup);
         }
     };
-
-    const goToNextPage = () => {
-        if (currentPage < totalPages - 1) {
-            onPageChange(currentPage + 1);
+    const goToNextBlock = () => {
+        if (currentBlock < totalBlocks - 1) {
+            setLoadPage((currentBlock+1) * pagesPerGroup);
         }
     };
 
     const goToPage = (page) => {
         if (page !== currentPage) {
-            onPageChange(page);
+            setLoadPage(page);
         }
     };
 
@@ -23,8 +31,8 @@ function Pagination({ totalPages, currentPage, onPageChange }) {
         <div className="flex items-center justify-center py-10 lg:px-0 sm:px-6 px-4">
             <div className="lg:w-3/5 w-full flex items-center justify-between border-t border-gray-200">
                 <div
-                    className={`flex items-center pt-3 text-gray-600 hover:text-sky-400 cursor-pointer ${currentPage === 0 && 'opacity-50 cursor-not-allowed'}`}
-                    onClick={goToPreviousPage}
+                    className={`flex items-center pt-3 text-gray-600 hover:text-sky-400 cursor-pointer ${currentBlock === 0 && 'opacity-50 cursor-not-allowed'}`}
+                    onClick={goToPreviousBlock}
                 >
                     <svg width="14" height="8" viewBox="0 0 14 8" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M1.1665 4H12.8332" stroke="currentColor" strokeWidth="1.25" strokeLinecap="round"
@@ -37,19 +45,19 @@ function Pagination({ totalPages, currentPage, onPageChange }) {
                     <p className="text-sm ml-3 font-medium leading-none">이전</p>
                 </div>
                 <div className="sm:flex hidden">
-                    {Array.from({ length: totalPages }, (_, i) => (
+                    {Array.from({ length: endPage - startPage }, (_, i) => (
                         <p
-                            key={i}
-                            onClick={() => goToPage(i )}
-                            className={`text-sm font-medium leading-none cursor-pointer text-gray-600 hover:text-sky-400 border-t ${currentPage === i  ? 'border-sky-400 text-sky-400' : 'border-transparent'} pt-3 mr-4 px-2`}
+                            key={startPage + i}
+                            onClick={() => goToPage(startPage + i)}
+                            className={`text-sm font-medium leading-none cursor-pointer text-gray-600 hover:text-sky-400 border-t ${currentPage === startPage + i ? 'border-sky-400 text-sky-400' : 'border-transparent'} pt-3 mr-4 px-2`}
                         >
-                            {i + 1}
+                            {startPage + i + 1}
                         </p>
                     ))}
                 </div>
                 <div
-                    className={`flex items-center pt-3 text-gray-600 hover:text-sky-400 cursor-pointer ${currentPage + 1 === totalPages && 'opacity-50 cursor-not-allowed'}`}
-                    onClick={goToNextPage}
+                    className={`flex items-center pt-3 text-gray-600 hover:text-sky-400 cursor-pointer ${currentBlock + 1 === totalBlocks && 'opacity-50 cursor-not-allowed'}`}
+                    onClick={goToNextBlock}
                 >
                     <p className="text-sm font-medium leading-none mr-3">다음</p>
                     <svg width="14" height="8" viewBox="0 0 14 8" fill="none" xmlns="http://www.w3.org/2000/svg">
