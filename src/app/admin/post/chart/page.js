952299fdc3b6242c67a66e01c010fda01d3dd1a9
henny <67935118+hykim123@users.noreply.github.com>
2024-09-25 13:49:21 +0900

    Update page.js

--- src/app/admin/post/chart/page.js
+++ src/app/admin/post/chart/page.js
@@ -24,10 +24,6 @@ export default function AdminDashboard() {
     const [location, setLocation] = useState([]);
     const [filteredPosts, setFilteredPosts] = useState([]);
 
-    window.addEventListener('load', () => {
-        getFilteredPosts("week");
-    });
-
     useEffect(() => {
         const fetchData = async () => {
             try {
@@ -41,6 +37,7 @@ export default function AdminDashboard() {
             }
         };
         fetchData();
+        getFilteredPosts("week");
     }, []);
 
     const getWeeklyData = () => {
