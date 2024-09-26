d29329f65d089240e03a9eb8ca5023e31e72b8eb
jong-jang <jjh0299@gmail.com>
2024-09-26 10:19:23 +0900

    Design : 헤더 반응형, 높이 스크린에 맞게 수정, 알림 추가

--- src/components/common/header/Header.js
+++ src/components/common/header/Header.js
@@ -1,7 +1,12 @@
 import Link from "next/link";
 import styles from "./Header.module.css";
+import {  FaUserPlus } from "react-icons/fa";
+import SystemAlert from "@/components/alert/SystemAlert";
+import {SlLogin} from "react-icons/sl"; // react-icons에서 아이콘 불러오기
 
 export default function Header() {
+	const isLoggedIn = false; // 로그인 여부 (임시)
+
 	return (
 		<header className={styles.header}>
 			<div className={styles.inner}>
@@ -17,9 +22,24 @@ export default function Header() {
 					</div>
 				</div>
 				<div className={styles.links}>
+					<SystemAlert />
+
+					{/* 데스크탑용 로그인, 회원가입 링크 */}
 					<Link href='/login'>로그인</Link>
 					<Link href='/reg'>회원가입</Link>
-					<Link href='/my'>마이페이지</Link>
+
+					{/* 로그인 상태에 따라 마이페이지 링크 표시 */}
+					{isLoggedIn && (
+						<Link href='/my'>마이페이지</Link>
+					)}
+
+					{/* 모바일용 아이콘 링크 */}
+					<Link href='/login' className={styles.iconLink}>
+						<SlLogin />
+					</Link>
+					<Link href='/reg' className={styles.iconLink}>
+						<FaUserPlus size={20} />
+					</Link>
 				</div>
 			</div>
 		</header>
