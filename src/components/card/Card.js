de544b13ad689ab1f66b0221333b1df78d0c3ab6
henny <hyeyoon4018@gmail.com>
2024-09-26 10:50:48 +0900

    Fix: 공고 상세보기 배너 추가

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -19,7 +19,7 @@ function Card({ item }) {
 			<div className='relative mb-4 rounded-2xl'>
 				<img
 					className='max-h-32 object-cover rounded-2xl w-full transition-transform duration-300 transform group-hover:scale-105'
-					src={`/s3/${item.company.coThumbimgUrl ?item.company.coThumbimgUrl : '1.jpg'}`}
+					src={`/s3/${item.company.coThumbimgUrl ? item.company.coThumbimgUrl : "1.jpg"}`}
 					width={"100"}
 					height={"100"}
 					alt='썸네일이미지'
@@ -33,7 +33,7 @@ function Card({ item }) {
 
 				<a
 					className='flex justify-center items-center bg-cyan-200 bg-opacity-80 z-10 absolute top-0 left-0 w-full h-full text-gray-700 rounded-2xl opacity-0 transition-all duration-300 transform group-hover:scale-105 text-xl group-hover:opacity-100'
-					onClick={() => router.push(`/user/post/view/${item.id}`)}
+					onClick={() => router.push(`/post/view/${item.id}`)}
 					target='_self'
 					rel='noopener noreferrer'
 				>
@@ -46,19 +46,22 @@ function Card({ item }) {
 			<div className='w-full mb-auto'>
 				<div>
 					<p className='text-l font-bold text-left mb-2'>{item.company.coName}</p>
-					<p className='text-m text-left mb-2' style={{
-						textOverflow: 'ellipsis',
-						overflow: 'hidden',
-						display: '-webkit-box',
-						WebkitLineClamp: 2,
-						WebkitBoxOrient: 'vertical'
-					}}>{item.poTitle}</p>
+					<p
+						className='text-m text-left mb-2'
+						style={{
+							textOverflow: "ellipsis",
+							overflow: "hidden",
+							display: "-webkit-box",
+							WebkitLineClamp: 2,
+							WebkitBoxOrient: "vertical",
+						}}
+					>
+						{item.poTitle}
+					</p>
 					<div className='text-sm text-gray-500 text-left flex justify-between'>
-						<span>{item.poDate}
-						</span>
+						<span>{item.poDate}</span>
 						<div className='text-sm flex items-center text-gray-500 '>
-							<svg className='ml-1 w-4 h-4' fill='none' stroke='currentColor' viewBox='0 0 24 24'
-								 xmlns='http://www.w3.org/2000/svg'>
+							<svg className='ml-1 w-4 h-4' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
 								<path d='M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z'></path>
 							</svg>
 							{deadlineDisplay}
