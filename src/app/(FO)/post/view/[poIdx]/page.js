20b5b37e235c8f436815b1ee6a10275e44b025d2
leesh9 <lokciginer@gmail.com>
2024-10-11 12:00:00 +0900

    지도 안움직이게 막아두기

--- src/app/(FO)/post/view/[poIdx]/page.js
+++ src/app/(FO)/post/view/[poIdx]/page.js
@@ -60,16 +60,6 @@ export default function ViewPage(props) {
 	}
 
 	function checkInterested() {
-		if (!login || login === "0") {
-			alert("로그인이 필요합니다.");
-			router.push("/login"); // 로그인 페이지 경로로 변경
-			return;
-		}
-
-		if (!role || role === "2" || role === "0") {
-			alert("지원할 수 없는 회원입니다.");
-			return;
-		}
 		axios({
 			url: `/api/interest/${posting.coIdx}`,
 			method: "get",
@@ -245,6 +235,16 @@ export default function ViewPage(props) {
 	};
 
 	const handleScrap = () => {
+		if (!login || login === "0") {
+			alert("로그인이 필요합니다.");
+			router.push("/login"); // 로그인 페이지 경로로 변경
+			return;
+		}
+
+		if (!role || role === "2" || role === "0") {
+			alert("지원할 수 없는 회원입니다.");
+			return;
+		}
 		setIsScrap(!isScrap);
 		axios({
 			url: `/api/scrap/${posting.id}`,
